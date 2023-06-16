b994708339d1044c3d339100b4e1e5650ff59661
JuanPabloArcilaLopez <112211363+JuanPabloArcilaLopez@users.noreply.github.com>
2023-06-15 20:59:01 -0500

    Update reducer.py

--- pregunta_01/reducer.py
+++ pregunta_01/reducer.py
@@ -2,17 +2,44 @@
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
 import sys
-import collections
+
+#
+# Esta funcion reduce los elementos que tienen la misma clave
+#
+import sys
 
 if __name__ == '__main__':
-  
-    counter = collections.Counter()
 
+    curkey = None
+    total = 0
+
+    #
+    # cada linea de texto recibida es una entrada clave \tabulador valor
+    #
     for line in sys.stdin:
-        line = line.strip()
-        key, val = line.split("\t")
-        val=int(val)
-        counter[key] += val
 
-    for data in counter.items():
-        sys.stdout.write("{}\t{}\n".format(data[0], data[1]))
+        key, val = line.split("\t")
+        val = int(val)
+
+        if key == curkey:
+            #
+            # No se ha cambiado de clave. Aca se acumulan los valores para la misma
+            # clave.
+            #
+            total += val
+        else:
+            #
+            # Se cambio de clave. Se reinicia el acumulador.
+            #
+            if curkey is not None:
+                #
+                # una vez se han reducido todos los elementos
+                # con la misma clave se imprime el resultado en
+                # el flujo de salida
+                #
+                sys.stdout.write("{}\t{}\n".format(curkey, total))
+
+            curkey = key
+            total = val
+
+    sys.stdout.write("{}\t{}\n".format(curkey, total))
