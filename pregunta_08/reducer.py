abe4ae4c37d08e1293236bfa0eec2377b105742d
JuanPabloArcilaLopez <112211363+JuanPabloArcilaLopez@users.noreply.github.com>
2023-06-15 21:08:45 -0500

    Update reducer.py

--- pregunta_08/reducer.py
+++ pregunta_08/reducer.py
@@ -1,3 +1,25 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+from collections import defaultdict
+import operator
+from traceback import StackSummary
+
+if __name__ == '__main__':
+
+    Dict = defaultdict(list)
+
+    for line in sys.stdin:
+
+        key, val = line.split("\t")
+        val = float(val)
+        Dict[key].append(val)
+
+    for clave, lista in Dict.items():
+        suma= round(sum(lista), 1)
+        prom = sum(lista)/len(lista)
+
+        valores = [clave, suma, prom]  
+           
+        sys.stdout.write("{}\t{}\t{}\n".format(valores[0], valores[1], valores[2]))
