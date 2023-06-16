0782002803751cc39115dc6347b5eba36839ab9c
JuanPabloArcilaLopez <112211363+JuanPabloArcilaLopez@users.noreply.github.com>
2023-06-15 20:59:26 -0500

    Update mapper.py

--- pregunta_02/mapper.py
+++ pregunta_02/mapper.py
@@ -3,5 +3,13 @@
 #
 import sys
 if __name__ == "__main__":
-    for line in sys.stdin:
-        sys.stdout.write("{}\t{}\n".format(line.split(',')[3],line.split(',')[4]))
+
+    my_iterator = iter(sys.stdin.readline, "")
+    next(my_iterator)
+
+    for line in my_iterator:
+        fields = line.strip().split(',',5)
+        purpose =  fields[3]
+        amount = fields[4]
+
+        sys.stdout.write("{}\t{}\n".format(purpose, amount))
