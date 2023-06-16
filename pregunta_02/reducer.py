ee606577a6e112e611f5a845ab13e196d8152d4d
JuanPabloArcilaLopez <112211363+JuanPabloArcilaLopez@users.noreply.github.com>
2023-06-15 21:00:04 -0500

    Update reducer.py

--- pregunta_02/reducer.py
+++ pregunta_02/reducer.py
@@ -3,10 +3,13 @@
 #
 import sys
 
+#
+# Esta funcion reduce los elementos que tienen la misma clave
+#
 if __name__ == '__main__':
 
     curkey = None
-    maximo = 0
+    mayor = 0
 
     #
     # cada linea de texto recibida es una entrada clave \tabulador valor
@@ -15,14 +18,14 @@ if __name__ == '__main__':
 
         key, val = line.split("\t")
         val = int(val)
-        
+
         if key == curkey:
             #
             # No se ha cambiado de clave. Aca se acumulan los valores para la misma
             # clave.
             #
-            if val > maximo:
-             maximo = val
+            if val > mayor:
+                mayor = val            
         else:
             #
             # Se cambio de clave. Se reinicia el acumulador.
@@ -33,9 +36,9 @@ if __name__ == '__main__':
                 # con la misma clave se imprime el resultado en
                 # el flujo de salida
                 #
-                sys.stdout.write("{}\t{}\n".format(curkey, maximo))
+                sys.stdout.write("{}\t{}\n".format(curkey, mayor))
 
             curkey = key
-            maximo = val
+            mayor = val
 
-    sys.stdout.write("{}\t{}\n".format(curkey, maximo))
+    sys.stdout.write("{}\t{}\n".format(curkey, mayor))
