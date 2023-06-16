2bed377913d60383132f8c3d89332ff41cfcee73
JuanPabloArcilaLopez <112211363+JuanPabloArcilaLopez@users.noreply.github.com>
2023-06-15 20:57:48 -0500

    Update mapper.py

--- pregunta_01/mapper.py
+++ pregunta_01/mapper.py
@@ -3,5 +3,13 @@
 #
 import sys
 if __name__ == "__main__":
+
+    #
+    # itera sobre cada linea de codigo recibida
+    # a traves del flujo de entrada
+    #
     for line in sys.stdin:
-            sys.stdout.write("{}\t1\n".format(line.split(',')[2]))
+        fields = line.strip().split(',',3)
+        credit_history =  fields[2]
+
+        sys.stdout.write("{}\t1\n".format(credit_history))
